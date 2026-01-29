c3b7d1317739c8adab7fbb28b7cde62ed0df040c
mr.Stepan <golenduhinsa@gmail.com>
2026-01-29 21:30:28 +0500

    -реализовал StatsClient

---
+++ stats/stats-client/src/main/java/ru/mrstepan/statsсlient/StatsApiClientImpl.java
@@ -0,0 +1,42 @@
+package ru.mrstepan.statsсlient;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import ru.mrstepan.RequestInfoDto;
+import ru.mrstepan.statsdto.EndpointStatDto;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class StatsApiClientImpl {
+    private final RestClient restClient;
+
+    public StatsApiClientImpl(RestClient.Builder builder) {
+        this.restClient = builder.baseUrl("https://localhost:9090")
+                .build();
+    }
+
+    public void hit(RequestInfoDto requestInfoDto) {
+        restClient.post()
+                .uri("/hit")
+                .body(requestInfoDto)
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    public Collection<EndpointStatDto> getStats(String start, String end, List<String> uris, Boolean unique) {
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("start", start)
+                        .queryParam("end", end)
+                        .queryParam("uris", uris)
+                        .queryParam("unique", unique)
+                        .path("/stats")
+                        .build())
+                .retrieve()
+                .body(new ParameterizedTypeReference<Collection<EndpointStatDto>>() {});
+    }
+}
