3e6dc3a90d8132e9894490ca7ae824fe5c9e6e1a
mr.Stepan <golenduhinsa@gmail.com>
2026-02-04 11:10:01 +0500

    -

--- stats/stats-client/src/main/java/ru/mrstepan/statsclient/StatsApiClientImpl.java
+++ stats/stats-client/src/main/java/ru/mrstepan/statsclient/StatsApiClientImpl.java
@@ -1,38 +1,47 @@
 package ru.mrstepan.statsclient;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-import ru.mrstepan.statsdto.RequestInfoDto;
+import ru.mrstepan.statsdto.GettingStatsParametersDto;
 import ru.mrstepan.statsdto.EndpointStatDto;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StatsApiClientImpl {
     private final RestClient restClient;
 
-    public StatsApiClientImpl(RestClient.Builder builder) {
-        this.restClient = builder.baseUrl("https://localhost:9090")
+    public StatsApiClientImpl(RestClient.Builder builder,
+                              @Value("${stats.service.url}") String statsServiceUrl) {
+        this.restClient = builder.baseUrl(statsServiceUrl)
                 .build();
     }
 
-    public void hit(RequestInfoDto requestInfoDto) {
+    public void hit(HttpServletRequest request) throws IOException {
+        BufferedReader reader = request.getReader();
+        String body = reader.lines().collect(Collectors.joining());
+
         restClient.post()
-                .uri("/hit")
-                .body(requestInfoDto)
+                .uri(request.getRequestURI())
+                .body(body)
                 .retrieve()
                 .toBodilessEntity();
     }
 
-    public Collection<EndpointStatDto> getStats(String start, String end, List<String> uris, Boolean unique) {
+    public Collection<EndpointStatDto> getStats(@Valid GettingStatsParametersDto gettingStatsDto) {
         return restClient.get()
                 .uri(uriBuilder -> uriBuilder
-                        .queryParam("start", start)
-                        .queryParam("end", end)
-                        .queryParam("uris", uris)
-                        .queryParam("unique", unique)
+                        .queryParam("start", gettingStatsDto.getStart())
+                        .queryParam("end", gettingStatsDto.getEnd())
+                        .queryParam("uris", gettingStatsDto.getUris())
+                        .queryParam("unique", gettingStatsDto.getUnique())
                         .path("/stats")
                         .build())
                 .retrieve()
--- stats/stats-dto/src/main/java/ru/mrstepan/statsdto/RequestInfoDto.java
+++ stats/stats-dto/src/main/java/ru/mrstepan/statsdto/RequestInfoDto.java
@@ -1,16 +1,27 @@
 package ru.mrstepan.statsdto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class RequestInfoDto {
+    @NotNull
+    @Max(255)
     private String app;
+
+    @NotNull
+    @Max(2048)
     private String uri;
+
+    @NotNull
+    @Max(45)
     private String ip;
 
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
