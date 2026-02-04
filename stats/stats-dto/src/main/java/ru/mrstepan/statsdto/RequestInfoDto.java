2be654ad03b1360f563f4452690a847c320e5435
mr.Stepan <golenduhinsa@gmail.com>
2026-02-04 11:23:23 +0500

    -fix

--- stats/stats-dto/src/main/java/ru/mrstepan/statsdto/RequestInfoDto.java
+++ stats/stats-dto/src/main/java/ru/mrstepan/statsdto/RequestInfoDto.java
@@ -1,8 +1,8 @@
 package ru.mrstepan.statsdto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,15 +10,15 @@ import java.time.LocalDateTime;
 @Data
 public class RequestInfoDto {
     @NotNull
-    @Max(255)
+    @Size(max = 255)
     private String app;
 
     @NotNull
-    @Max(2048)
+    @Size(max = 2048)
     private String uri;
 
     @NotNull
-    @Max(45)
+    @Size(max = 45)
     private String ip;
 
     @NotNull
