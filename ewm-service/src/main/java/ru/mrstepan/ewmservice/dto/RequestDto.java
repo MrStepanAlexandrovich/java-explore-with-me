c547600f81abe7ce5551e149401ccce26bdcb628
mr.Stepan <golenduhinsa@gmail.com>
2026-02-16 21:02:32 +0500

    -fix

--- ewm-service/src/main/java/ru/mrstepan/ewmservice/dto/RequestDto.java
+++ ewm-service/src/main/java/ru/mrstepan/ewmservice/dto/RequestDto.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RequestDto {
-    private String created;
+    private LocalDateTime created;
     private Long event;
     private Long id;
     private Long requester;
--- ewm-service/src/main/java/ru/mrstepan/ewmservice/model/RequestMapper.java
+++ ewm-service/src/main/java/ru/mrstepan/ewmservice/model/RequestMapper.java
@@ -5,11 +5,9 @@ import ru.mrstepan.ewmservice.dto.RequestDto;
 import java.time.format.DateTimeFormatter;
 
 public class RequestMapper {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
-
     public static RequestDto toDto(Request request) {
         return new RequestDto(
-                request.getCreated() != null ? request.getCreated().format(FORMATTER) : null,
+                request.getCreated() != null ? request.getCreated() : null,
                 request.getEvent() != null ? request.getEvent().getId() : null,
                 request.getId(),
                 request.getRequester() != null ? request.getRequester().getId() : null,
